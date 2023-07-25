99fe9cfbed2e9098866e18d4a64ed9fc5a765306
PathofKaze <pathofkaze@gmail.com>
2023-07-25 10:21:37 +0800

    Update EmailSmtp.py
    Implementation of task scheduler and mock db

--- Email/EmailSmtp.py
+++ Email/EmailSmtp.py
@@ -1,7 +1,36 @@
 from email.mime.multipart import MIMEMultipart
-import smtplib, ssl
+import smtplib, ssl #pip install secure-smtplib
 import getpass
+import schedule #pip install schedule
+import time
+import pymysql.cursors #pip install pymysql
 
+#Definition of functions
+def NotifyEmail():
+    try:
+        server.sendmail(username, notification, msg.as_string())
+    finally:
+        server.quit()
+
+def ProbeDB():
+        connnection = pymysql.connect(host='localhost',
+                                     user='user',
+                                     password='12345',
+                                     database='db',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        with connnection:
+            try:
+                with connnection.cursor() as cursor:
+                    sql = "SELECT `users`"
+                    cursor.execute(sql)
+            except:
+                NotifyEmail()
+         
+def sched_job():
+     schedule.every(900).seconds.do(ProbeDB) #schedule job every 15 minutes
+     return schedule.CancelJob
+
+#User input
 print("Welcome to the Database monitoring system DBMS.\nPlease login to the system email to begin the notification to the system")
 username = input("Please enter your email:")
 password = getpass.getpass(prompt = "Please enter your password:")
@@ -10,7 +39,7 @@ notification = input("Please enter the email that you would like to receive serv
 port = 465
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+with smtplib.SMTP_SSL(domain, port, context=context) as server:
         server.login (username, password)
 
 msg = MIMEMultipart('The database is currently down.')
@@ -18,9 +47,10 @@ msg['To'] = notification
 msg['From'] = username
 msg['Subject'] = 'Production database instance is currently not working'
 
-try:
-    server.sendmail(username, notification, msg.as_string())
-finally:
-    server.quit()
+schedule.every().day.at('00:00').do(sched_job)
+
+while True:
+     schedule.run_pending()
+     time.sleep(1)
 
 
